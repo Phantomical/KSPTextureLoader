408a3ff3ae185e049bd18b55cd4d5c9e6d696948
Phantomical <phantom@lynches.ca>
2025-12-06 15:55:54 -0800

    Add method overloads without options

--- src/KSPTextureLoader/TextureLoader.cs
+++ src/KSPTextureLoader/TextureLoader.cs
@@ -99,6 +99,33 @@ public partial class TextureLoader : MonoBehaviour
     }
 
     /// <summary>
+    /// Load a texture from disk or from an asset bundle.
+    /// </summary>
+    /// <typeparam name="T">A specific texture type to load.</typeparam>
+    /// <param name="path">The path to load the texture from on disk or in asset bundles.</param>
+    ///
+    /// <remarks>
+    /// Depending on the type you pick for <typeparamref name="T"/> there are several
+    /// texture conversions that can happen:
+    ///
+    /// <list type="bullet">
+    ///   <item>
+    ///     <see cref="Cubemap"/>: can be converted from a <see cref="Texture2D"/> with the faces
+    ///     packed horizontally in a cross configuration.
+    ///   </item>
+    ///   <item>
+    ///     <see cref="Texture2DArray"/>: can be converted from a <see cref="Texture2D"/>. This
+    ///     will result in an array texture with depth 1.
+    ///   </item>
+    /// </list>
+    ///
+    /// If you are passing the texture directly into a shader property then usually you should be
+    /// able to get away with leaving <typeparamref name="T"/> as <see cref="Texture"/>.
+    /// </remarks>
+    public static TextureHandle<T> LoadTexture<T>(string path)
+        where T : Texture => LoadTexture<T>(path, new());
+
+    /// <summary>
     /// Check whether a texture exists.
     /// </summary>
     /// <param name="path">A path to load the texture from on disk or in asset bundles.</param>
@@ -120,6 +147,27 @@ public partial class TextureLoader : MonoBehaviour
     /// </remarks>
     public static bool TextureExists(string path, TextureLoadOptions options) =>
         Instance.DoTextureExists(path, options);
+
+    /// <summary>
+    /// Check whether a texture exists.
+    /// </summary>
+    /// <param name="path">A path to load the texture from on disk or in asset bundles.</param>
+    /// <returns>Whether a texture with that name exists on disk or in an asset bundle.</returns>
+    ///
+    /// <remarks>
+    /// With implicit bundles it can be rather difficult to tell whether a
+    /// texture actually exists to be loaded. This method is meant to bridge
+    /// that gap. It does not actually load the texture, but it will load asset
+    /// bundles that might contain the texture.
+    ///
+    /// Note that just because the texture exists that doesn't mean that it
+    /// will load successfully. Don't use this to check just before loading
+    /// the texture. Instead you should just try to load the texture.
+    ///
+    /// This is meant for load-time checks so that you can display a warning to
+    /// the user if their install is incorrect.
+    /// </remarks>
+    public static bool TextureExists(string path) => TextureExists(path, new());
 
     // Use weak references so that textures that get leaked will at least get
     // cleaned up during a scene switch.
