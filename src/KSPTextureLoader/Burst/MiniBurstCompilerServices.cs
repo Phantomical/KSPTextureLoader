f52cd718230804245336619437163ea0887dec64
Phantomical <phantom@lynches.ca>
2026-07-10 12:42:33 -0700

    Ship an ILPacked version of Unity.Burst

--- src/KSPTextureLoader/Burst/MiniBurstCompilerServices.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Unity.Burst.CompilerServices;
-
-[AttributeUsage(AttributeTargets.Method)]
-internal class IgnoreWarningAttribute : Attribute
-{
-    public IgnoreWarningAttribute(int warning) { }
-}
